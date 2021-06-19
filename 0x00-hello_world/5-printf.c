88afedf4fbe35465fc31d67669881d1f56c1f500
Omenix <omenigph@gmail.com>
2021-06-18 20:57:14 -0700

    printf function is ready

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is used to print with proper grammar,
+ * but the outcome is a piece of art,
+ *
+ * Return: the function would return 0 if it prints
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
