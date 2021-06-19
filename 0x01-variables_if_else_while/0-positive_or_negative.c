43f323e4aa5ffff148c315331504799a6e67d0a8
Omenix <omenigph@gmail.com>
2021-06-19 13:07:33 -0700

    if condition statement project

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - this function is used to print polarity of values
+ *
+ *
+ * Return: these returns 0 if the code printf displays
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	printf("%d is positive", n);
+	}
+	else if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+	/* your code goes there */
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+* __ this is the first file that is used define the if else statement__
+* 0-positive_or_negative is a code that is used to assign predicate to random values
